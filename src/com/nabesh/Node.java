6723d159a78fcd6aa567d1abbf56ffa8b9b32e82
KariukiAbel <abelmuriithi@gmail.com>
2020-03-15 12:58:37 +0300

    Learning about Binary Tree data structures and printing node elements through inOrderTraversal

---
+++ src/com/nabesh/Node.java
@@ -0,0 +1,62 @@
+package com.nabesh;
+
+public class Node {
+    Node left, right;
+    int data;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public void insert(int value){
+        if (value <= data){
+            if(left == null){
+                left = new Node(value);
+            }else{
+                left.insert(value);
+            }
+        }else{
+            if (right == null){
+                right = new Node(value);
+            }else{
+                right.insert(value);
+            }
+        }
+    }
+
+    public boolean contains(int value){
+        if (value == data){
+            return true;
+        }else if (value < data){
+             if (left == null){
+                 return false;
+             }else{
+                 return left.contains(value);
+             }
+            }else {
+            if (right == null){
+                return false;
+            }else{
+                return right.contains(value);
+            }
+        }
+    }
+
+    public void printInOrderTraversal(){
+        if (left != null){
+            left.printInOrderTraversal();
+        }
+        System.out.println(data);
+        if(right != null){
+            right.printInOrderTraversal();
+        }
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        Node node = new Node(8);
+        node.insert(5);
+        node.contains(3);
+        node.printInOrderTraversal();
+    }
+}
