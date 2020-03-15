e76ceb22300912fc4020fc13e98d0351434723e7
KariukiAbel <abelmuriithi@gmail.com>
2020-03-15 13:15:06 +0300

    Done the pre-order tree traversal

--- src/com/nabesh/Node.java
+++ src/com/nabesh/Node.java
@@ -63,12 +63,29 @@ public class Node {
         System.out.println(data);
     }
 
+    //In pre-order traversal the root node gets printed first followed by left then the right
+    public void printPreOrderTraversal(){
+        System.out.println(data);
+        if (left != null){
+            left.printPreOrderTraversal();
+        }
+        if (right != null){
+            right.printPreOrderTraversal();
+        }
+    }
+
     public static void main(String[] args) {
 	// write your code here
         Node node = new Node(8);
         node.insert(5);
+        node.insert(50);
+        node.insert(7);
+        node.insert(2);
         node.contains(3);
         node.printInOrderTraversal();
+        System.out.println("Below shows the nodes in post order traversal");
         node.printPostOrderTraversal();
+        System.out.println("Below shows the nodes in pre-order traversal");
+        node.printPreOrderTraversal();
     }
 }
